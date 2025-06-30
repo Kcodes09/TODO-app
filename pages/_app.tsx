5f8dff80cc4e28dba0b53d49b89733b8af1875ca
Kcodes09 <keshavgiridharan09@gmail.com>
2025-06-30 22:44:42 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <TaskProvider>
       <Head>
         <title>TODO App</title>
-        <link rel="icon" type="image/x-icon" href="../public/Favicon.png" />
+        <link rel="icon" type="image/x-icon" href="/Favicon.png" />
       </Head>
       <div className="min-h-screen bg-gray-100 text-black">
         <Header />
