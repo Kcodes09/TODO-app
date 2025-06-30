4763cd1a2130dcc3a16a39a279be0a6c947388b3
Kcodes09 <keshavgiridharan09@gmail.com>
2025-06-30 22:33:11 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import { TaskProvider } from "../context/TaskContext";
@@ -6,6 +7,10 @@ import { TaskProvider } from "../context/TaskContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <TaskProvider>
+      <Head>
+        <title>TODO App</title>
+        <link rel="icon" href="/Favicon.png" />
+      </Head>
       <div className="min-h-screen bg-gray-100 text-black">
         <Header />
         <main className="container mx-auto px-4 py-6">
